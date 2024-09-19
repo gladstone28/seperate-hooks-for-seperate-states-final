070203eed88af4c02e08d10afe50419378864f70
Gladstone Rose <gladstonerose@hotmail.com>
2024-09-19 15:30:47 -0500

    add project files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
+import React from "react";
+import SocialNetwork from "./SocialNetwork";
 
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-<h1>Gladstone is a champion react developer</h1>
-    </div>
-  );
+export default function App() {
+  return <SocialNetwork />;
 }
-
-export default App;
